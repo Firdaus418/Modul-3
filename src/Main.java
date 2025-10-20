ccbf2eb2c80983b665cc8628d20c55ea36ea238d
FirdausUMM <firdausfirmansyahemah@gmail.com>
2025-10-20 07:01:15 +0700

    init push

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Arays;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int[n] arr = {13, 7, 6, 45, 21, 9, 2, 100};
+        Arrays.sort(arr, 1, 5);
+        System.out.printf("Modified arr[] : %s",
+                Arrays.toString(arr));
+    }
+}
+
