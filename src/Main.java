d5e53e2a31252bedf5d51bcca9c7c4287b617fd1
FirdausUMM <firdausfirmansyahemah@gmail.com>
2025-10-20 07:22:55 +0700

    debugging 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
-import java.util.Arays;
+import java.util.Arrays;
 public class Main
 {
     public static void main(String[] args)
     {
-        int[n] arr = {13, 7, 6, 45, 21, 9, 2, 100};
+        int[] arr = {13, 7, 6, 45, 21, 9, 2, 100};
         Arrays.sort(arr, 1, 5);
         System.out.printf("Modified arr[] : %s",
                 Arrays.toString(arr));
